dd53c0716927e2b0851e00ca235e994aa4862150
Tim Marston <tim@ed.am>
2023-09-02 15:19:53 +0100

    updated scema v1 and fixed sqlite code

--- starsd/store/sqlite.v
+++ starsd/store/sqlite.v
@@ -22,18 +22,18 @@ pub fn (mut s SqliteDB) update() ! {
 		for version := current + 1; version <= schema_version; version++ {
 			println('db: applying ${version:04}.sql')
 			file := os.resource_abs_path('./store/schema/${version:04}.sql')
-			mut sql := ''
+			mut raw_sql := ''
 			for line in os.read_lines(file)! {
 				part := line.trim_space()
 				if part.len > 0 && (part.len < 2 || part[0..2] != '--') {
-					sql += part
-					if sql#[-1..] == ';' {
-						s.db.exec(sql)!
-						sql = ''
+					raw_sql += if raw_sql.len > 0 { ' ${part}' } else { part }
+					if raw_sql#[-1..] == ';' {
+						s.db.exec(raw_sql)!
+						raw_sql = ''
 					}
 				}
 			}
-			if sql != '' {
+			if raw_sql != '' {
 				return error('missing ; at end of file')
 			}
 			s.db.exec('PRAGMA user_version=${version}')!
