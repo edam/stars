fe5da9f5a9d42d7a96b8c873cc31bbe73869ace1
marnukei <marnukei@waxworlds.org>
2023-09-02 16:17:48 +0100

    updated data in schema v1

--- starsd/store/schema/0001.sql
+++ starsd/store/schema/0001.sql
@@ -17,20 +17,30 @@ CREATE TABLE stars (
   got BOOL
 );
 
+-- wins
+CREATE TABLE wins (
+  id INTEGER PRIMARY KEY ASC NOT NULL,
+  at STRING NOT NULL,
+  got BOOL,
+  prize_id INTEGER NOT NULL
+);
+
+--
+
 INSERT INTO prizes VALUES (1, 2, 300, '2023-08-18', NULL);
 
 INSERT INTO stars VALUES (NULL, '2023-08-19', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-20', 0, 1, false);
 INSERT INTO stars VALUES (NULL, '2023-08-21', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-21', 1, 1, false);
--- got weekly win too!
+INSERT INTO wins VALUES (NULL, '2023-08-21', true, 1);
 
 INSERT INTO stars VALUES (NULL, '2023-08-26', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-27', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-28', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-29', 0, 1, true);
 INSERT INTO stars VALUES (NULL, '2023-08-29', 1, 1, true);
--- got weekly win too!
+INSERT INTO wins VALUES (NULL, '2023-08-29', true, 1);
 
 INSERT INTO stars VALUES (NULL, '2023-09-02', 0, 1, NULL);
 INSERT INTO stars VALUES (NULL, '2023-09-03', 0, 1, NULL);
@@ -40,3 +50,4 @@ INSERT INTO stars VALUES (NULL, '2023-09-06', 0, 1, NULL);
 INSERT INTO stars VALUES (NULL, '2023-09-07', 0, 1, NULL);
 INSERT INTO stars VALUES (NULL, '2023-09-08', 0, 1, NULL);
 INSERT INTO stars VALUES (NULL, '2023-09-08', 1, 1, NULL);
+INSERT INTO wins VALUES (NULL, '2023-09-08', NULL, 1);
