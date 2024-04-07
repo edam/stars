84c3055d2744209da13e664b002decfe18ef8aa2
Tim Marston <tim@ed.am>
2024-04-07 20:25:25 +0100

    webapp: added title

--- webapp/src/components/Title.jsx
+++ webapp/src/components/Title.jsx
@@ -1,7 +1,34 @@
+import { useContext } from 'react';
+import { Avatar, Dropdown } from "flowbite-react";
+import { AuthContext } from '@/contexts/Auth';
+import { HiCog, HiLogout } from "react-icons/hi";
+
+
 export function Title() {
+  const { username, logout } = useContext( AuthContext );
+
   return (
-    <div className="dbg">
-      <h1>STARS</h1>
+    <div className="bg-white drop-shadow-xl">
+      <div className="flex max-w-screen-lg mx-auto items-center">
+        <h1 className="grow text-2xl font-extrabold m-2">Daily Stars</h1>
+        <div className="hover:bg-gray-100">
+          <Dropdown
+            arrowIcon={ false }
+            inline
+            label={
+              <Avatar rounded className="m-2" />
+            }>
+            <Dropdown.Header>Hi, { username }!</Dropdown.Header>
+            <Dropdown.Divider />
+            <Dropdown.Item icon={ HiCog }>
+              Settings
+            </Dropdown.Item>
+            <Dropdown.Item icon={ HiLogout } onClick={ logout }>
+              Logout
+            </Dropdown.Item>
+          </Dropdown>
+        </div>
+      </div>
     </div>
   );
 }
